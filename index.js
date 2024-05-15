9fe7b199c5261bdf680974d05dab3f363653b8f5
teeshihqun <teeshihqun@gmail.com>
2024-05-15 10:14:48 +0800

    chore: Update MongoDB connection and retrieve orders

--- index.js
+++ index.js
@@ -33,8 +33,59 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    //await client.db("admin").command({ ping: 1 });
+    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Connected correctly to server");
+    const db = client.db('test'); // Define 'db' here
+
+
+    const pipeline = [
+            {
+              $match:
+                /**
+                 * query: The query in MQL.
+                 */
+                {
+                  name: "Leslie Martinez",
+                },
+            },
+            {
+              $project:
+                /**
+                 * specifications: The fields to
+                 *   include or exclude.
+                 */
+          
+                {
+                  
+                  name: 1,
+                  email: 1,
+                  accounts: 1
+                },
+            },
+            {
+              $lookup:
+                /**
+                 * from: The target collection.
+                 * localField: The local join field.
+                 * foreignField: The target join field.
+                 * as: The name for the results.
+                 * pipeline: Optional pipeline to run on the foreign collection.
+                 * let: Optional variables to use in the pipeline field stages.
+                 */
+                {
+                  from: "accounts",
+                  localField: "accounts",
+                  foreignField: "account_id",
+                  as: "accounts",
+                },
+            },
+
+    ];
+
+    const orders = await client.db('lab2').collection('sample_analytics').aggregate(pipeline).toArray();
+    console.log(orders);
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
